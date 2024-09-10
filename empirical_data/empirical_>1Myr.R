71493829fb3e263461697af9d1944aa854c44409
Vilde Bruhn Kinneberg <35314975+VildeBruhn@users.noreply.github.com>
2024-09-10 12:43:16 +0200

    Update empirical_>1Myr.R

--- empirical_data/empirical_>1Myr.R
+++ empirical_data/empirical_>1Myr.R
@@ -29,7 +29,7 @@ library(broom.mixed)
 ## REMEMBER TO CHANGE PATH TO FILES ## 
 ######################################
 
-PATH = "../../../../GitHub/rate_time/empirical_data/"
+PATH = "PATH_TO_DATA_FOLDER"
 
 # import functions
 source(paste0(PATH, "empirical_functions.R"))
